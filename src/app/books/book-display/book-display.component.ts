0a6a4ddfcd4adc934b58d08300ce910f58a26b38
Jess0908 <jessica.hunger@icloud.com>
2024-06-07 13:19:41 +0200

    Angepasst

--- src/app/books/book-display/book-display.component.ts
+++ src/app/books/book-display/book-display.component.ts
@@ -9,16 +9,21 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./book-display.component.scss']
 })
 export class BookDisplayComponent implements OnInit {
-  public books: Book[];
-
+  
+  public books: Book[] = [
+    new Book(1, '123', 'Buch 1', 'Springer', 'Autor 1', new Date('2020-01-01'), false),
+    new Book(2, '456', 'Buch 2', 'O\'Reilly', 'Autor 2', new Date('2019-06-15'), true),
+    new Book(3, '789', 'Buch 3', 'Penguin', 'Autor 3', new Date('2018-08-23'), false),
+    new Book(4, '012', 'Buch 4', 'Random House', 'Autor 4', new Date('2021-03-10'), true)
+  ];
   constructor(private service: BookService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.service.getAll().subscribe({
+   /* this.service.getAll().subscribe({
       next: (data) => {
         this.books = data;
       }
-    })
+    })*/
   }
 
   public btnDeleteClicked(element: Book) {
