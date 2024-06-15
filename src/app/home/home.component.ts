806e5a34e92288bf9e62ec5a40865ed4afc9fbf5
Fynia97 <annabel@gaul05.de>
2024-06-15 23:00:39 +0200

    [Add] Register and login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { LoggedInUser } from '../login/loggedInUser.model';
+import { LoginService } from '../login/login.service';
+import { UserService } from '../users/user.service';
+import { User } from '../users/user.model';
+import { BookService } from '../books/book.service';
+import { LendService } from '../lends/lend.service';
+import { Lend } from '../lends/lend.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public currentUser$: Observable<LoggedInUser | null> = of(null);
+  public user: User;
+  public numberOfBooks: number;
+  public numberOfLends: number;
+  public numberOfLendsRequests: number;
+  public nextLendFrom: Lend;
 
+  constructor(private loginService: LoginService, private userService: UserService, private bookService: BookService, private lendService: LendService) { }
+
+  ngOnInit(): void {
+    this.currentUser$ = this.loginService.currentUser$;
+
+    this.SetUser();
+    this.CountBooks();
+    this.CountLends();
+    this.NextLendFrom();
+    this.GetNumberOfLends();
+    this.GetNumberOfLendsRequests();
+  }
+
+  public SetUser() {
+    this.currentUser$.subscribe((loggedInUser: LoggedInUser | null) => {
+      if (loggedInUser) {
+        this.userService.getByEmail(loggedInUser.email).subscribe({
+          next: (data) => {
+            this.user = data;
+          }
+        });
+      }
+    });
+  }
+
+  public CountBooks() {
+    this.bookService.getAll().subscribe({
+      next: (data) => {
+        this.numberOfBooks = data.length;
+      }
+    })
+  }
+
+  public CountLends() {
+    this.lendService.getAll().subscribe({
+      next: (data) => {
+        this.numberOfLends = data.length;
+      }
+    })
+  }
+
+  public NextLendFrom() {
+    this.lendService.getNextLendFrom().subscribe({
+      next: (data) => {
+        this.nextLendFrom = data;
+      }
+    })
+  }
+
+  private GetNumberOfLends() {
+    this.lendService.countLendsWithStatus(0).subscribe((count) => {
+    this.numberOfLends = count;
+    });
+    }
+    
+    private GetNumberOfLendsRequests() {
+    this.lendService.countLendsWithStatus(4).subscribe((count) => {
+    this.numberOfLendsRequests = count;
+    });
+    }
 }
--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { AbstractService } from '../common/abstract.service';
 import { User } from './user.model';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService extends AbstractService<User> {
   protected name = "UserProfile";
+
+  public getByEmail(email: string): Observable<User> {
+    return this.client.get<User>(AbstractService.baseUrl + this.name + '/getByEmail?email=' + email);
+  }
 }
